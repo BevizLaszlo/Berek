cebd79b74dfbe2844ac1ffae99c014636f5b37a3
Bevíz László <bevizlaci@gmail.com>
2024-11-19 09:24:58 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,22 @@
+﻿namespace Berek;
+internal class Ber
+{
+    public string Nev { get; set; }
+    public bool Nem { get; set; }
+    public string Reszleg { get; set; }
+    public int Belepes { get; set; }
+    public int Berezes { get; set; }
+
+    public Ber(string sor)
+    {
+        string[] adat = sor.Split(';');
+
+        Nev = adat[0];
+        Nem = adat[1].ToLower() == "férfi";
+        Reszleg = adat[2];
+        Belepes = int.Parse(adat[3]);
+        Berezes = int.Parse(adat[4]);
+    }
+
+    public override string ToString() => $"{Nev}({(Nem ? "férfi" : "nő")}) - {Reszleg} {Belepes} {Berezes}Ft";
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Kezdés: 9:00
+ * Végzés: 9:24
+ */
+
+using Berek;
+using System.Text;
+
+List<Ber> berek = [];
+using (StreamReader sr = new StreamReader(path: @"..\..\..\src\berek2020.txt", encoding: Encoding.UTF8))
+{
+    _ = sr.ReadLine();
+    while (!sr.EndOfStream)
+        berek.Add(new Ber(sr.ReadLine()));
+}
+
+Console.WriteLine($"3. feladat: Összesen {berek.Count} dolgozó adata található a forrásállományban.");
+Console.WriteLine($"4. feladat: Az átlagbér {(double)(berek.Average(b => b.Berezes) / 1000):.0} ezer Ft");
+Console.Write("5. feladat: Adja meg a részleg nevét: ");
+string reszleg = Console.ReadLine();
+
+Console.Write("6. feladat: ");
+if (berek.Any(r => r.Reszleg == reszleg))
+    Console.WriteLine($"A legnagyobb bérű dolgozó a részlegen: {
+        berek.Where(r => r.Reszleg == reszleg)
+             .OrderByDescending(b => b.Berezes)
+             .First().ToString()}");
+else
+    Console.WriteLine("A megadott részleg nem rendelkezik a cégnél!");
+
+Console.WriteLine("7. feladat: Részlegen dolgozók száma:");
+foreach (IGrouping<string, Ber> resz in berek.GroupBy(r => r.Reszleg))
+    Console.WriteLine($"\t{resz.Key}: {resz.Count()} fő");
